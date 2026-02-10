689a085537faf18d43a82bddd3def050d112dd79
Lucy Whalley <l.whalley@northumbria.ac.uk>
2026-02-10 13:48:47 +0000

    implemented code needed to extract projected dos, calculate temperature dependent stats, and store as nested dict

--- phonon_sonification/MP_interface.py
+++ phonon_sonification/MP_interface.py
@@ -3,12 +3,17 @@ import numpy as np
 import math
 import time
 import yaml
+import scipy
 from scipy import constants
 import os
 import argparse
- 
+from dotenv import load_dotenv
+
 sample_rate = 44100
-api_string = None # insert API key here
+
+load_dotenv () # use python-dotenv library for storing secrets in a .env file in project route (or at another path that is specified here)
+min_audible = 20# minimum audible frequency in herz
+max_audible = 8E3 # maximum audible frequency in herz
 
 # this function adapted from
 # https://stackoverflow.com/questions/73494717/trying-to-play-multiple-frequencies-in-python-sounddevice-blow-horrific-speaker
@@ -117,13 +122,15 @@ def gamma_frequencies_from_mesh(phonon_mesh_filepath):
 
 def process_imaginary(phonon_frequencies):
     # remove any imaginary modes
+    phonon_frequencies = list(phonon_frequencies)
     phonon_cleaned_frequencies = [frequency for frequency in phonon_frequencies if frequency > 0]
     if len(phonon_frequencies) != len(phonon_cleaned_frequencies):
         print("There are {} imaginary frequencies which have not been processed".format(len(phonon_frequencies)-len(phonon_cleaned_frequencies)))
-    return phonon_cleaned_frequencies
+    return np.array(phonon_cleaned_frequencies)
 
 def process_imaginary_dos(dos,phonon_frequencies) :
     # remove dos which correspond to imaginary modes
+    phonon_frequencies = list(phonon_frequencies)
     dos_cleaned_frequencies = [dos[i] for i in range(len(phonon_frequencies)) if phonon_frequencies[i] > 0]
 
     return np.array(dos_cleaned_frequencies)
@@ -135,45 +142,108 @@ def gamma_frequencies_from_mp_id(mp_id):
     import mp_api
     from mp_api.client import MPRester
 
-    with MPRester(api_string) as mpr:
+    with MPRester(os.getenv('MP_API_KEY')) as mpr:
         try:
-            bs = mpr.materials.phonon.get_data_by_id(mp_id).ph_bs
+            bs = mpr.get_phonon_bandstructure_by_material_id(mp_id)
         except:
             print("this materials project entry does not appear to have phonon data")
             pass
-
     print("extracting frequencies for qpoint {}".format(bs.qpoints[0].cart_coords))
 
-    phonon_frequencies = list(bs.bands[:,0]*1E12)   # convert from THz to Hz
+    phonon_frequencies = list(bs.to_pmg.bands[:,0*1E12])   # convert from THz to Hz
     phonon_frequencies = process_imaginary(phonon_frequencies)
     print("phonon frequencies are (Hz):", phonon_frequencies)
 
     return phonon_frequencies
 
 def dos_data_from_mp_id(mp_id):
-    """return frequencies (in Hz) at which density of states is evaluated, and the density of states itself. This is for a material hosted on the Materials Project.
+    """return dos data obect. This is for a material hosted on the Materials Project.
     Material is identified using unique ID number. Note that to use this feature you need a Materials
     Project API key (https://materialsproject.org/api)."""
     import mp_api
     from mp_api.client import MPRester
-
-    with MPRester(api_string) as mpr:
+    with MPRester(os.getenv('MP_API_KEY')) as mpr:
 
         try: 
-            dos = mpr.materials.phonon.get_data_by_id(mp_id).ph_dos
+            dos = mpr.get_phonon_dos_by_material_id(mp_id)
         except:
             print("this materials project entry does not appear to have phonon data")
             pass
 
-        phonon_frequencies = list(np.array(dos.as_dict()['frequencies'])*1E12) # convert from THz to Hz 
-        phonon_frequencies = process_imaginary(phonon_frequencies)    
+    return dos
 
-        dos = dos.as_dict()['densities']
-        dos = process_imaginary_dos(dos,phonon_frequencies) 
+def get_dos_raw(mp_id):
+    """get the full and projected densities. Return as a nested dictionary. Arg is the materials project ID. """
+    dos = dos_data_from_mp_id(mp_id)
+
+    dos_dict = {}
+    dos_dict['metadata'] = {'mp_id' : mp_id}
+    dos_dict['projection'] = {}
+
+    frequencies = np.array(dos.frequencies)*1E12 # convert from THz to Hz
+    frequencies = process_imaginary(frequencies)  
     
-    return phonon_frequencies, dos
+    densities = process_imaginary_dos(dos.densities,frequencies) 
+    dos_dict['projection']['all'] = {'densities': densities,
+                                     'frequencies': frequencies}
+            
+    for i,site in enumerate(dos.structure.relabel_sites().sites):
+        densities = process_imaginary_dos(dos.projected_densities[i],frequencies) 
+        dos_dict['projection'][site.label] = {'densities': densities,
+                                                        'frequencies': frequencies} 
+    return dos_dict
+    
+def dos_stats_analysis(mp_id,temp=None):
+    """for each entry in a dos_dict, calculate the integrated dos, the phonon band centre, the quantiles and the IQR and of the dos distribution in Hz (discounting any negative frequencies) and add these to the nested dicts. The dos is optionally weighted by Bose Einstein occupation at a specified temp."""
+    if 0 in temp:
+        print ("cannot calculate stats for 0K")
+        temp.remove(0)
+    dos_dict = get_dos_raw(mp_id)
+    for site in dos_dict['projection']:
+        site_dict = dos_dict['projection'][site]
+        site_dict['stats'] = {}
+        site_dict['stats']['athermal'] = {}
+        densities = site_dict['densities']
+        f = site_dict['frequencies']
+        site_dict['stats']['athermal']['band_centre'] = phonon_band_centre(f,densities)
+        site_dict['stats']['athermal']['integrated_dos'] = integrated_dos(f,densities)
+        site_dict['stats']['athermal']['quantile_25'] = weighted_quantile(f, densities, 0.25)
+        site_dict['stats']['athermal']['quantile_75'] = weighted_quantile(f, densities, 0.75)
+        site_dict['stats']['athermal']['IQR'] = phonon_dos_IQR(f,densities)
+        
+        if temp:
+            site_dict['stats']['thermal'] = {}
+            if not hasattr(temp, '__iter__'):
+                temp = [temp]    # if single temp provided as scalar, convert it to iterable list
+            for t in temp:
+                densities_scaled = scale_by_occupation(densities, f, t)
+                site_dict['stats']['thermal'][str(t)] = {}
+                site_dict['stats']['thermal'][str(t)]['band_centre'] = phonon_band_centre(f,densities_scaled)
+                site_dict['stats']['thermal'][str(t)]['integrated_dos'] = integrated_dos(f,densities_scaled)
+                site_dict['stats']['thermal'][str(t)]['quantile_25'] = weighted_quantile(f, densities_scaled, 0.25)
+                site_dict['stats']['thermal'][str(t)]['quantile_75'] = weighted_quantile(f, densities_scaled, 0.75)
+                site_dict['stats']['thermal'][str(t)]['IQR'] = phonon_dos_IQR(f,densities_scaled)
 
+    return dos_dict
 
+def dict_to_df(dos_dict):
+    """convert the nested dictionary to a pandas dataframe"""
+    dos_df = 2
+    
+    return dos_df
+    
+def phonon_band_centre(f,dos):
+    """for each particular chemical species, get the phonon band centre in Hz (discounting any negative frequencies). Return as a dictionary with species string as key."""
+    return scipy.integrate.simpson(dos*f,f)/scipy.integrate.simpson(dos,f)
+    
+def integrated_dos(f,dos):
+    """for each particular chemical species, get the integrated dos (discounting any negative frequencies). Return as a dictionary with species string as key."""
+    return scipy.integrate.simpson(dos,f)
+
+def phonon_dos_IQR(f,dos):
+    """return the inter-quartile range of the dos distribution"""    
+    return weighted_quantile(f, dos, 0.75) - weighted_quantile(f, dos, 0.25)
+    
 def bose_einstien_distribution(energy,temperature):
     return 1 / (math.exp(energy/(constants.Boltzmann*temperature)) - 1)
 
@@ -182,15 +252,37 @@ def frequency_to_energy(frequency):
     energy = constants.h*frequency
     return energy
 
-def scale_by_occupation(amplitudes, phonon_frequencies, temperature):
-    """Re-scale the amplitude by the Bose Einstein occupation factor for a state with energy h*frequency at specified temperature."""
+def scale_by_occupation(densities, phonon_frequencies, temperature):
+    """Re-scale the density by the Bose Einstein occupation factor for a state with energy h*frequency at specified temperature."""
     BE_occupations = np.array([bose_einstien_distribution(frequency_to_energy(frequency),temperature) 
                            for frequency in phonon_frequencies])
 
     # scale by occupation expressed as a fraction of the total occupation
-    scaled_amplitudes = amplitudes*(BE_occupations/max(BE_occupations))
+    scaled_densities = densities*BE_occupations
 
-    return scaled_amplitudes
+    return scaled_densities
+
+def weighted_quantile(values, weights, quantile):
+    """
+    Compute a weighted quantile of discrete data.
+    
+    values   : array-like data points
+    weights  : array-like weights (same length as values)
+    quantile : float in [0,1] (e.g. 0.25, 0.75)
+    """
+    values = np.asarray(values)
+    weights = np.asarray(weights)
+
+    # sort by values
+    sorter = np.argsort(values)
+    values = values[sorter]
+    weights = weights[sorter]
+
+    # cumulative distribution
+    cum_weights = np.cumsum(weights)
+    cum_weights /= cum_weights[-1]
+
+    return np.interp(quantile, cum_weights, values)
 
 def play_chord(timelength):
     """DRONE POWER"""
@@ -251,7 +343,10 @@ def main(args):
             amplitudes = get_athermal_amplitudes(num_frequencies=len(phonon_frequencies))
 
         else:
-            phonon_frequencies, dos = dos_data_from_mp_id(mp_id)
+            dos = dos_data_from_mp_id(mp_id)
+            data_dict = full_dos_data(dos)
+            phonon_frequencies = data_dict['frequencies']
+            dos = data_dict['densities']
             amplitudes = get_athermal_amplitudes(dos_array=dos,num_frequencies=len(phonon_frequencies))
         
         # Convert phonon frequencies to audible frequencies (return in Hz)
